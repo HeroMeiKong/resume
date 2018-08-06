d367758204ccc7fb661fbcbec0495047066d6b96
xiaopang <xiaopangfeng4@hotmail.com>
2018-08-07 01:09:04 +0800

    delete oldresume

--- New/scripts/main.js
+++ New/scripts/main.js
@@ -1,3 +1,7 @@
 setTimeout(function () {
   WelcomeAnimation.classList.remove('active');
 }, 1000);
+let subbtn = document.querySelectorAll('#subbtn')
+subbtn.addEventListener('submit',function(){
+  this.location.href="url"
+})
