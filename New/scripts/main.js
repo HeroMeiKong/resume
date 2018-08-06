d7e7b77c99207347fdca5b8e2aa6788312535e4b
xiaopang <xiaopangfeng4@hotmail.com>
2018-08-07 01:42:11 +0800

    delete oldindex

--- New/scripts/main.js
+++ New/scripts/main.js
@@ -1,7 +1,12 @@
 setTimeout(function () {
   WelcomeAnimation.classList.remove('active');
 }, 1000);
-let subbtn = document.querySelectorAll('#subbtn')
-subbtn.addEventListener('submit',function(){
-  this.location.href="url"
-})
+function a() {
+  let subbtn = document.querySelectorAll('#subbtn')
+  console.log(subbtn)
+  subbtn.addEventListener('click', (x) => {
+    console.log(1)
+    
+  })
+}
+a()
